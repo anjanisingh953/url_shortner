9ea2c58f075364445394cdab45eb047a913b0ead
Anjani Singh <anjanisingh953@gmail.com>
2025-09-29 23:56:03 +0530

    Resolved the dotenv injecting issue

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 require('dotenv').config()
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
-app.get('/test',(req,res)=>{
-    res.status(200).json({msg:'testing'})
+mongoose.connect(process.env.MONGO_URI)
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', async(req,res)=>{
+    const shortUrls = await ShortUrl.find();
+    res.render('index',{shortUrls})
+})
+
+app.post('/shortUrls', async(req,res)=>{
+    await ShortUrl.create({full : req.body.fullUrl, });
+    res.redirect('/')
+})
+
+app.get('/:shortUrl',async(req,res)=>{
+    const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl});
+    if(shortUrl == null) return res.sendStatus(404);
+    shortUrl.clicks++;
+    shortUrl.save();
+
+    res.redirect(shortUrl.full)
 })
 
 module.exports = app;
